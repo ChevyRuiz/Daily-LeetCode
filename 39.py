a0373e3f152e5fb744cde3bf6e35998c22c76137
ChevyRuiz <78135824+ChevyRuiz@users.noreply.github.com>
2025-12-28 15:50:28 -0500

    Combination Sum

---
+++ 39.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def dfs(i):
+            if sum(subset) == target:
+                res.append(subset.copy())
+                return
+            if i >= len(candidates) or sum(subset) > target:
+                return
+            subset.append(candidates[i])
+            dfs(i)
+            subset.pop()
+            dfs(i + 1)
+        
+        dfs(0)
+        return res
